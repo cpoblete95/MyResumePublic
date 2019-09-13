25367ba214f3681f4e6c0387c79154ba2acbfd31
Christopher Poblete <poblete.christopher95@gmail.com>
2019-09-12 21:41:22 -0400

    Added custom font and banner

--- src/Pages/LandingPage/LandingPage.js
+++ src/Pages/LandingPage/LandingPage.js
@@ -1,8 +1,10 @@
 import React ,{useEffect} from 'react';
-
 import styles from './LandingPage.css';
-import Avatar from '../../../Images/Avatar.png';
-import Intro from '../../../Images/Intro.png';
+
+import Avatar from '../../Images/Avatar.png';
+import Intro from '../../Images/Intro.png';
+
+import Banner from '../../components/Banner/Banner';
 
 
 const LandingPage = () => {
@@ -15,7 +17,7 @@ const LandingPage = () => {
         <div className = {styles.LandingPage}>
             <img className = {styles.AvatarImg} src = {Avatar}></img>
             <img className = {styles.IntroImg} src = {Intro}></img>
-
+            <Banner/>
             {/* <h1>I'm Christopher.</h1> */}
 
         </div>
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './Banner.css';
+
+const Banner = () => {
+
+    return(
+        <div className = {classes.Banner}>
+            <h1>Software Developer</h1>
+            <hr/>
+            <p>HTML/CSS | Java | Javascript | React | AngularJS</p>
+        </div>
+    )
+}
+
+export default Banner;
